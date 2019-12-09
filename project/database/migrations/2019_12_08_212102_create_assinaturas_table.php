cf895dfd0d6882c13748e7f78c57da0df61b61ab
CassiaNardoni <32914019+CassiaNardoni@users.noreply.github.com>
2019-12-08 23:59:07 -0300

    add migration assinaturas

---
+++ project/database/migrations/2019_12_08_212102_create_assinaturas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssinaturasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assinaturas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('solicitacao_id');
+            $table->unsignedBigInteger('user_destino');
+            $table->unsignedBigInteger('documento_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assinaturas');
+    }
+}
